ed96719c35b21fb50c280435a503ce4209e264b5
YOBO Rayane <yoborayane24@gmail.com>
2023-07-08 12:42:01 +0100

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+
 
 function App() {
+  const firebaseConfig = {
+  apiKey: "AIzaSyDZe_XJ-GB_YA3VfbqIY9A895a6hhytsKg",
+  authDomain: "mon-projet-9e0d3.firebaseapp.com",
+  projectId: "mon-projet-9e0d3",
+  storageBucket: "mon-projet-9e0d3.appspot.com",
+  messagingSenderId: "681500379395",
+  appId: "1:681500379395:web:e27f10308569118578c136"
+};
+firebase.initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
